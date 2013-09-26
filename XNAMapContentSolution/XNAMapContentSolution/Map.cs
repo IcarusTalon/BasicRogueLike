7e759687f1e7b9ef2d30206481f7615654e9ea25
J.G. Taylor <josephgtaylor@live.com>
2013-09-26 15:45:25 -0600

    Generated Test Map

--- XNAMapContentSolution/XNAMapContentSolution/Map.cs
+++ XNAMapContentSolution/XNAMapContentSolution/Map.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System.IO;
 
 namespace XNAMapContentSolution
 {
@@ -20,32 +21,84 @@ namespace XNAMapContentSolution
             
             _mapTiles = new MapTile[_rowNumber, _columnNumber];
 
-            for (int row = 0; row < _rowNumber; row++)
+
+            string mapTemp = string.Empty;
+            string[] mapRowsTemp;
+            string[] mapColumnTemp;
+
+            MapTileType currentTile;
+            using (StreamReader streamReader = new StreamReader("MapTest1.txt"))
             {
-                for (int column = 0; column < _columnNumber; column++)
+                mapTemp = streamReader.ReadToEnd();
+                mapRowsTemp = mapTemp.Split('\n');
+                mapColumnTemp = mapRowsTemp[0].Split(',');
+                
+
+                _mapTiles = new MapTile[mapRowsTemp.Count(), mapColumnTemp.Count()];
+                Random random = new Random();
+
+                for (int i = 0; i < mapRowsTemp.Count(); i++)
                 {
-                    _mapTiles[row, column] = new MapTile();
-                    if (row == 1 && column == 1)
+                    mapColumnTemp = mapRowsTemp[i].Split(',');
+                    for (int j = 0; j < mapColumnTemp.Count(); j++)
                     {
-                        _mapTiles[row, column].Initialize(FloorTiles.GreentTile, new Rectangle(), new Rectangle[]{new Rectangle()}, new Rectangle(row * 32, column * 32, 32, 32));
-                    }
-                    else
-                    {
-                        _mapTiles[row, column].Initialize(WallTiles.GreentWalls[0], new Rectangle(column * 32, row * 32, 32, 32));
-                    }
+                        currentTile = (MapTileType)int.Parse(mapColumnTemp[j].Trim());
+                        Rectangle positionRectangle = new Rectangle(j * 32, i * 32, 32, 32);
+                        _mapTiles[i, j] = new MapTile();
 
+                        switch(currentTile)                        
+                        {
+                                //Walls
+                            case MapTileType.GreenWall:
+                                if (random.Next(0, 10) == 0)
+                                {
+                                    _mapTiles[i, j].Initialize(WallTiles.GreentWalls[random.Next(0, 20)], positionRectangle);
+                                }
+                                else
+                                {
+                                    _mapTiles[i, j].Initialize(WallTiles.GreentWalls[0], positionRectangle);
+                                }
+                                break;
+                                
+                            
+                            //Floors
+                            case MapTileType.GreenFloor:
+                                _mapTiles[i, j].Initialize(FloorTiles.GreentTile, new Rectangle(), new Rectangle[] { new Rectangle() }, positionRectangle);
+                                break;
+
+                        }
+                        
+                    }
+                    
                 }
-
             }
+
+            //for (int row = 0; row < _rowNumber; row++)
+            //{
+            //    for (int column = 0; column < _columnNumber; column++)
+            //    {
+            //        _mapTiles[row, column] = new MapTile();
+            //        if (row == 1 && column == 1)
+            //        {
+            //            _mapTiles[row, column].Initialize(FloorTiles.GreentTile, new Rectangle(), new Rectangle[]{new Rectangle()}, new Rectangle(row * 32, column * 32, 32, 32));
+            //        }
+            //        else
+            //        {
+            //            _mapTiles[row, column].Initialize(WallTiles.GreentWalls[0], new Rectangle(column * 32, row * 32, 32, 32));
+            //        }
+
+            //    }
+
+            //}
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
 
 
-            for (int row = 0; row < _rowNumber; row++)
+            for (int row = 0; row < _mapTiles.GetLength(0); row++)
             {
-                for (int column = 0; column < _columnNumber; column++)
+                for (int column = 0; column < _mapTiles.GetLength(1); column++)
                 {
                     //switch (_mapTiles[row, column].MapType)
                     //{
